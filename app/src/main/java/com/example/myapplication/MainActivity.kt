3320f397203eff00807f5803a74d54646a318857
Harshit Roy <101564851+harshit-roy@users.noreply.github.com>
2023-10-13 00:59:41 +0530

    Minor Fix

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,43 +1,46 @@
-package com.example.myapplication
+package com.example.todo
 
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
-class MainActivity : ComponentActivity() {
+
+// entity - table
+// dao - queries
+// database
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var database: myDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+        database = Room.databaseBuilder(
+            applicationContext, myDatabase::class.java, "To_Do"
+        ).build()
+        add.setOnClickListener {
+            val intent = Intent(this, CreateCard::class.java)
+            startActivity(intent)
         }
+        deleteAll.setOnClickListener {
+            DataObject.deleteAll()
+            GlobalScope.launch {
+                database.dao().deleteAll()
+            }
+            setRecycler()
+        }
+
+        setRecycler()
+
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("roy")
+    fun setRecycler() {
+        recycler_view.adapter = Adapter(DataObject.getAllData())
+        recycler_view.layoutManager = LinearLayoutManager(this)
     }
 }
